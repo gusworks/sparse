2c8c6a50e68c19ebed699236a7c3c5b5f8df614c
pbalduino <pbalduino+github@gmail.com>
2011-07-16 16:25:04 -0300

    Removed sparse_spec.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ require 'sparse'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-Dir["#{File.dirname(__FILE__)}/shared/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   
